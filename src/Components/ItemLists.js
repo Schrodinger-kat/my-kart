275ad0ebaf1201e83979454a433f1aeee31b0344
jishnn.tp <jishnn.tp@gmail.com>
2020-04-17 21:58:56 +0530

    added stuffs

---
+++ src/Components/ItemLists.js
@@ -0,0 +1,48 @@
+const stuffs = {
+  item1: {
+    name: "Nivea Football",
+    image: "/images/football.jpg",
+    desc:
+      "Nivea Superstar 2017 classic football, handstiched for hard grounds.",
+    price: 900,
+    status: "available"
+  },
+
+  item2: {
+    name: "Yamaha PSR-E263",
+    image: "/images/yamaha.jpg",
+    desc:
+      "Yamaha PSR-E263 digital keyboard is a must for every keyboard enthusiast.",
+    price: 7700,
+    status: "available"
+  },
+
+  item3: {
+    name: "Adibas EQT Bask",
+    image: "/images/adibas.jpg",
+    desc:
+      "The EQT franchise is a modern throwback to pure sport function, seen on the runways of the latest fashion shows and in the hip-hop scenes.",
+    price: 1999,
+    status: "unavailable"
+  },
+
+  item4: {
+    name: "Samsung Galaxy S9+",
+    image: "/images/s9.jpg",
+    desc:
+      "A blend of innovative design and technology - the Samsung Galaxy S9+ is powered by an octa-core processor, which runs all its innovative features seamlessly. A brilliant camera and nifty features such as augmented reality, Super Slow-mo, and Live Translation will revolutionise the way you've been using a smartphone.",
+    price: 26000,
+    status: "available"
+  },
+
+  item5: {
+    name: "Real Madrid Body Spray",
+    image: "/images/rm.jpg",
+    desc:
+      "Real Madrid, Eau de Toilette. Natural Spray. Hola Madrid !",
+    price: 1000,
+    status: "available"
+  }
+};
+
+export default stuffs;
